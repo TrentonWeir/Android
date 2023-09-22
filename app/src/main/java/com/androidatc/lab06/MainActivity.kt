5d50da8621fea2d06c70a89308ff7b4536a6728b
Trenton Weir <81660030+TrentonWeir@users.noreply.github.com>
2023-09-22 16:31:35 -0400

    Page 6-46

--- app/src/main/java/com/androidatc/lab06/MainActivity.kt
+++ app/src/main/java/com/androidatc/lab06/MainActivity.kt
@@ -2,10 +2,22 @@ package com.androidatc.lab06
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.androidatc.lab06.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
+    fun display(view: View) {
+        binding.info.text = "Android Application Development, Android Security Essentials, and Monetize Android Applications"
+    }
+
+
 }
